feb177806797d322b98ea7faeaddceb0d52d5033
Suberhaibe <hibba04@hotmail.se>
2023-01-27 10:33:18 +0100

    Add project files.

---
+++ Uppgift 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift5._4
+{
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Walter", "Tim", "Adam", "Ameen", "Freja" };
+            Array.Sort(namn);
+            for(int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
